929145542a12848eb17a9a66fb0d215507ecb611
Jay Love <jsloveiv@gmail.com>
2017-03-18 11:57:49 -0400

    syntax cleanup

--- xap_controller.py
+++ xap_controller.py
@@ -1,3 +1,4 @@
+# pylint: disable = E0126
 """
 Support for interfacing with ClearOne XAP800 and XAP400 units via serial port.
 
@@ -5,26 +6,26 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.xap_controller/
 """
 
-testing=0
-
-DOMAIN = 'xap_controller'
-
 import logging
 import voluptuous as vol
-import time
 
 from homeassistant.components.media_player import (
-    SUPPORT_TURN_OFF, SUPPORT_TURN_ON, SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_SET,
-    SUPPORT_SELECT_SOURCE, MediaPlayerDevice, PLATFORM_SCHEMA, MEDIA_TYPE_MUSIC)
+    SUPPORT_TURN_OFF, SUPPORT_TURN_ON, SUPPORT_VOLUME_MUTE,
+    SUPPORT_VOLUME_SET, SUPPORT_SELECT_SOURCE, MediaPlayerDevice,
+    PLATFORM_SCHEMA, MEDIA_TYPE_MUSIC)
 
 from homeassistant.const import (
-    CONF_HOST, CONF_PORT, STATE_OFF, STATE_ON, STATE_UNKNOWN, STATE_IDLE, CONF_NAME)
+    STATE_OFF, STATE_ON, CONF_NAME)
 
 import homeassistant.helpers.config_validation as cv
 
-#REQUIREMENTS = [
+REQUIREMENTS = [
     'https://github.com/jslove/XAPX00/archive/0.1.1.zip'
     '#XAPX00==0.1.1']
+
+testing = 0
+
+DOMAIN = 'xap_controller'
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -35,8 +36,10 @@ CONF_STEREO  = 'stereo'
 
 SRC_OFF = 'Off'
 
-SUPPORT_XAP_ZONE   = SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_SET | \
-                     SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_SELECT_SOURCE
+SUPPORT_XAP_ZONE = \
+                   SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_SET | \
+                   SUPPORT_TURN_ON | SUPPORT_TURN_OFF | \
+                   SUPPORT_SELECT_SOURCE
 
 SUPPORT_XAP_SOURCE = SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_SET | \
                      SUPPORT_TURN_ON | SUPPORT_TURN_OFF
@@ -47,12 +50,13 @@ OUTPUT_SCHEMA = vol.Schema({
 })
 
 SOURCE_SCHEMA = vol.Schema({
-    cv.string:int,
+    cv.string: int,
 })
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PATH): cv.string,
-    vol.Required(CONF_ZONES): vol.Schema({cv.string: vol.All(cv.ensure_list,[int])}),
+    vol.Required(CONF_ZONES): vol.Schema({cv.string:
+                                          vol.All(cv.ensure_list, [int])}),
     vol.Required(CONF_SOURCES): SOURCE_SCHEMA,
 })
 
@@ -70,10 +74,10 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     xapconn = XAPX00(path)
 
-    if config.get(CONF_STEREO,1)==0:
-        xapconn.stereo=0
+    if config.get(CONF_STEREO, 1) == 0:
+        xapconn.stereo = 0
     else:
-        xapconn.stereo=1
+        xapconn.stereo = 1
 
     xapconn.convertDb = 1
     xapconn.connect()
@@ -85,9 +89,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
                     hass, xapconn, source_name, source_input)])
     else:
         _LOGGER.error('Not connected to %s', path)
-        
-    sources[SRC_OFF]=0
 
+    sources[SRC_OFF] = 0
 
     if xapconn.connected:
         for zone_name, outputs in config[CONF_ZONES].items():
@@ -101,7 +104,7 @@ class XAPSource(MediaPlayerDevice):
     """
     Represents one source
     """
-    
+
     def __init__(self, hass, xapconn, source_name, source_input, unitCode=0):
         """Initialise the XAPX00 source pseudo-device"""
         _LOGGER.debug("Setting Up Source %s" % source_name)
@@ -116,12 +119,12 @@ class XAPSource(MediaPlayerDevice):
     def name(self):
         """Return the name of the source."""
         return "Source: " + self._name
-    
+
     @property
     def state(self):
         """Return the state of the device."""
         return self._state
-    
+
     @property
     def supported_features(self):
         """Flag of media commands that are supported."""
@@ -136,16 +139,17 @@ class XAPSource(MediaPlayerDevice):
     def is_volume_muted(self):
         """ return bool mute status"""
         return bool(self._isMuted)
-        
+
     def get_volume_level(self):
         """Volume level of the media player (0..1)."""
         gain = self._xapx00.getPropGain(self._input, group="I")
         self._volume = gain
         return self._volume
-    
+
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
-        volume = self._xapx00.setPropGain(self._input, volume, isAbsolute=1, group="I")
+        volume = self._xapx00.setPropGain(self._input, volume,
+                                          isAbsolute=1, group="I")
         self._volume = volume
 
     def turn_on(self):
@@ -157,7 +161,8 @@ class XAPSource(MediaPlayerDevice):
     def turn_off(self):
         """Turn off media player."""
         if self._xapx00.getMute(self._input) == 0:
-            self._isMuted = self._xapx00.setMute(self._input, group="I", isMuted=1)
+            self._isMuted = self._xapx00.setMute(self._input, group="I",
+                                                 isMuted=1)
         self._state = STATE_OFF
 
     def mute_volume(self, mute):
@@ -168,7 +173,7 @@ class XAPSource(MediaPlayerDevice):
         self._isMuted = self._xapx00.getMute(self._input, group="I")
         return self._isMuted
 
-        
+
 class XAPZone(MediaPlayerDevice):
     """
     Represents one or more XAP outputs, either mono or stereo
@@ -178,15 +183,16 @@ class XAPZone(MediaPlayerDevice):
         self._name = zone_name
         self._xapx00 = xapconn
         self._unitCode = unitCode
-        self._sources = sources  #a dict, source name to input #
+        self._sources = sources  # a dict, source name to input #
         self._sources_reverse = {v: k for k, v in sources.items()}
-        self._outputs = outputs  #a list of ouputs
-        self._volume=0
-        self._defaultMatrixLevel=1
+        self._outputs = outputs  # a list of ouputs
+        self._volume = 0
+        self._defaultMatrixLevel = 1
         self._isMuted = self.get_mute_status()
         self._active_source = SRC_OFF
         self._active_source = self.get_source()
-        self.select_source(self._active_source)  #make sure sources synced across outputs
+        # make sure sources synced across outputs
+        self.select_source(self._active_source)
         self._sync_volume_level()
         self._state = STATE_ON if self._active_source != SRC_OFF else STATE_OFF
         _LOGGER.info("zone {} set up".format(zone_name))
@@ -196,17 +202,17 @@ class XAPZone(MediaPlayerDevice):
         self.get_mute_status()
         self.get_source()
         self.get_volume_level()
- 
+
     @property
     def name(self):
         """Return the name of the zone."""
         return "Zone: " + self._name
-    
+
     @property
     def state(self):
         """Return the state of the device."""
         return self._state
-    
+
     @property
     def supported_features(self):
         """Flag of media commands that are supported."""
@@ -230,48 +236,55 @@ class XAPZone(MediaPlayerDevice):
     def is_volume_muted(self):
         """ return mute status"""
         return bool(self._isMuted)
-    
+
     def setDefaultLevel(self):
         """  set all crosspoint levels to default """
         for xOut in self._outputs:
             for source in self._sources:
-                volume = self._xapx00.getMatrixLevel(self._sources[source],
-                                                     self._outputs[xOut], self._defaultMatrixLevel)
-    
+                self._xapx00.setMatrixLevel(self._sources[source],
+                                            self._outputs[xOut],
+                                            self._defaultMatrixLevel)
+
     def clear_matrix(self):
         """ set all crosspoints to off"""
         for xOut in self._outputs:
             for xIn in self._xapx00.input_range:
-                self._xapx00.setMatrixRouting(xIn,xOut,0)
-    
+                self._xapx00.setMatrixRouting(xIn, xOut, 0)
+
     def get_source(self):
         """ Get first active source for outputs in this zone """
         _LOGGER.debug("In get_source")
         for i in self._sources.keys():
             if i != SRC_OFF:
-                z_state = self._xapx00.getMatrixRouting(self._sources[i], self._outputs[0])
-                _LOGGER.debug("matrix rounting for {}={}".format(i,z_state))
+                z_state = self._xapx00.getMatrixRouting(self._sources[i],
+                                                        self._outputs[0])
+                _LOGGER.debug("matrix rounting for {}={}". format(i, z_state))
                 if z_state == 1:
                     self._active_source = i
                     break
-            #if testing:
-            #    actsrc = self._sources_reverse[1]
+            # if testing:
+            #     actsrc = self._sources_reverse[1]
         _LOGGER.debug("get_source = %s" % self._active_source)
         return self._active_source
-    
+
     def _sync_volume_level(self):
-        """set all level of all outputs in zone to the same level as the first one in zone"""
+        """set all level of all outputs in zone to the same
+        level as the first one in zone"""
         if self._active_source != SRC_OFF:
-            volume = self._xapx00.getPropGain(self._outputs[0], group="O", unitCode=self._unitCode)
-            #volume = self._xapx00.getMatrixLevel(self._sources[self._active_source], self._outputs[0])
+            volume = self._xapx00.getPropGain(self._outputs[0], group="O",
+                                              unitCode=self._unitCode)
+            # volume = self._xapx00.getMatrixLevel(self._sources[
+            # self._active_source], self._outputs[0])
             self.set_volume_level(volume)
-    
+
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
         for output in self._outputs:
             _LOGGER.debug("Set Volume for output {}".format(output))
-            volume = self._xapx00.setPropGain(output, volume, group="O", unitCode=self._unitCode)
-            #self._xapx00.setMatrixLevel(self._sources[self._active_source], output, volume)
+            volume = self._xapx00.setPropGain(output, volume, group="O",
+                                              unitCode=self._unitCode)
+            # self._xapx00.setMatrixLevel(self._sources[self._active_source],
+            # output, volume)
         self._volume = volume
 
     def get_volume_level(self):
@@ -280,7 +293,6 @@ class XAPZone(MediaPlayerDevice):
         self._volume = gain
         return self._volume
 
-        
     def turn_on(self):
         """Turn zone on"""
         self._state = STATE_ON
@@ -297,33 +309,35 @@ class XAPZone(MediaPlayerDevice):
     def mute_volume(self, mute=2):
         """Send mute command, mute is bool from hass, default is 2 (toggle)"""
         for output in self._outputs:
-            muted = self._xapx00.setMute(output, group="O", isMuted=int(mute), unitCode=self._unitCode)
+            muted = self._xapx00.setMute(output, group="O", isMuted=int(mute),
+                                         unitCode=self._unitCode)
         self._isMuted = bool(muted)
 
     def get_mute_status(self):
         self._isMuted = bool(self._xapx00.getMute(self._outputs[0], group="O"))
         return self._isMuted
-            
+
     def select_source(self, source):
         """Set the input source"""
         actsrc = self._active_source
         for xOut in self._outputs:
             if actsrc != SRC_OFF:
-                self._xapx00.setMatrixRouting(self._sources[actsrc],xOut,0)
+                self._xapx00.setMatrixRouting(self._sources[actsrc], xOut, 0)
             self._active_source = SRC_OFF
             if source in self._sources and source != SRC_OFF:
-                self._xapx00.setMatrixRouting(self._sources[source],xOut,1)
+                self._xapx00.setMatrixRouting(self._sources[source], xOut, 1)
                 self._active_source = source
 
     @property
     def source_list(self):
         """List of available input sources."""
-        _LOGGER.debug("Source List, Active Source: {}".format(self._active_source))
+        _LOGGER.debug("Source List, Active Source: {}".format(
+            self._active_source))
         return list(self._sources.keys())
 
     @property
     def source(self):
         """ Current source"""
-        _LOGGER.debug("property: source: {}".format(self._active_source))        
+        _LOGGER.debug("property: source: {}".format(self._active_source))
         return self._active_source
 
