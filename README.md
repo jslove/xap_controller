48ff1c61e370a1888a9167e490a78bbecead6a9b
Jay Love <jsloveiv@gmail.com>
2017-07-24 22:16:45 -0400

    Update documentation for multiple unit configuration

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 ## The XAPController platform allows the use of a ClearOne XAP 400 or 800 unit as an audio routing matrix with HomeAssistant.
 
 At this time, it is set up only for routing audio, not for use with microphones.
@@ -9,17 +8,20 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.xap_controller/
 (no docs there until release)
 
-
 There are two components in the platform: output Zones and input Sources.  The input sources are assumed to be XAP type I inputs, 
-the output zones type O outputs.  The system can assume that the channels are set up for stereo, so that there are 2 channels paired 
+the output zones type O outputs. 
+
+Sources and zones can soecify only the input (output) channel, in which case it is assumed they are on unit 0.  They can also specify the unit, and optionally, for sources, an expansion bus that the source is also mapped to, so that it can be used on other units in the systems.  In this case, list the unit:Channel of the source, and then the expansion channel and optinoally expansion group (E or P).  When sources or zones are specified with units and or expansin bused, the items should be listed as a string of the format "<Unit#>:<Channel#>:<Expansion Bus Channel Letter>:<Expansion BUs Group>".  See below for an example.  For the expansion bus setup to work the matrix needs to have the sources mapped to the expansin channels through the G-Ware software.
+
+The system can assume that the channels are set up for stereo, so that there are 2 channels paired 
 together.  If stereo=1, the module will take each action twice, once on the listed source/zone number and again on the source/zone + 1.
 
-For each source or zone, indicate the channel to use on the XAP unit.
+For each source or zone, multiple channels can be listed, as a list.  If multiple channels are listed for a source and an output, they will be paired sequentially, source item 1 to zeone item 1, source item 2 to zone irem 2, etc.  If there are more source channels than zone channels, only the first channels in the source will be used.  If there are more channels in a zone than in the source being applied ot it, the source channels will be repeated.  This multiple channel apporach can be used to handle stereo (instead of the stereo=1 approach), but it was added to handle surround sound sources / zones. 
 
 The platform will create individual media_player controls for each source and zone.  Each source will be shown with a volume slider, adjusting the 
-gain for that input.  Each Zone will be shown with a dropbox to select from the available sources and a volume slider to adjust the gain for that output.
+gain for that input.  Each Zone will be shown with a dropbox to select from the available zones and a volume slider to adjust the gain for that output.
 
-Basic configuration:
+Basic configuration
 ```
 media_player:
    - platform: xap_controller
@@ -40,16 +42,61 @@ media_player:
        'Living/Dining/Library':
          - 9
        'WorkRoom':
-         - 11
+         - "2:1"
      sources:
        'Home Audio': 9
        'Family TV Audio': 11
 ```
 
-* name: the name of the platform instance
-* path: serial device path (can be a virtual serial port, using socat for example)
-* baud: baud rate of serial port, default=38400
-* stereo: 1=stereo, 0=mono  If stereo=1, each action will be performed twice on the output and output+1
-* zones: a list of output zone names, with a list one or more outputs for each zone
-* sources: a list of source names, with a list of one source input number per source name.
-* scan_interval: how often to scan the unit for changes
+Multi-unit example
+```
+media_player:
+   - platform: xap_controller
+     path: /dev/ttyUSB-XAP800
+     name: MyXAP
+     scan_interval: 30
+     stereo: 0
+     baud: 9600
+     zones:
+       'Office':
+         - "1:1"
+         - "1:2"
+       'Kitchen':
+         - "1:3"
+         - "1:4"
+        'Family Room Surround':
+         - "2:1"
+         - "2:2"
+         - "2:3"
+         - "2:4"
+         - "2:1"
+         - "2:2"
+         # Family Room Surround has no center channel, so list the two front 
+         # speakers at the end and map the center channel to each of them
+
+     sources:
+       'Home Audio':
+         - "1:9:O:E"
+         - "1:10:P:E"
+       'Family TV Surround Audio':
+         - "2:1:V:E"
+         - "2:2:W:E"
+         - "2:3:X:E"
+         - "2:4:Y:E"
+         - "2:5:Z:E"
+         - "2:5:Z:E"
+         # Family Room Surround has no center channel, so list the center channel twice at the end,  
+         # then it will be connected to the last 2 itens in the zone channel list
+```
+
+zones: a list of output zone names, with a list one or more outputs for each zone. 
+sources: a list of source names, with a list of one or more sources per source name.
+ sources are listed as either a digit, indicating the input channel on unit 0, or else a string of the 
+format:  "<unit#>:<input#>:<bus letter>:<bus type>. Bus and Bus type are optional, but are neeed if using more than 1
+ unit and you want a source to be available on outputs in other units. 
+path: serial device path (can be a virtual serial port, using socat for example)
+name: the name of the platform instance
+stereo: 1=stereo, 0=mono  If stereo=1, each action will be performed twice on the input (output) and input (output)+1
+baud: baud rate of serial port, default=38400
+scan_interval: how often to scane the unit for changes
+
--- xap_controller.py
+++ xap_controller.py
@@ -4,19 +4,23 @@ Support for interfacing with ClearOne XAP800 and XAP400 units via serial port.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.xap_controller/
+< not there yet >
 
 
 There are two components in the platform: output Zones and input Sources.  The input sources are assumed to be XAP type I inputs, 
-the output zones type O outpurs.  The system can assume that the channels are set up for stereo, so that there are 2 channels paired 
+the output zones type O outputs. 
+
+Sources and zones can soecify only the input (output) channel, in which case it is assumed they are on unit 0.  They can also specify the unit, and optionally, for sources, an expansion bus that the source is also mapped to, so that it can be used on other units in the systems.  In this case, list the unit:Channel of the source, and then the expansion channel and optinoally expansion group (E or P).  When sources or zones are specified with units and or expansin bused, the items should be listed as a string of the format "<Unit#>:<Channel#>:<Expansion Bus Channel Letter>:<Expansion BUs Group>".  See below for an example.  For the expansion bus setup to work the matrix needs to have the sources mapped to the expansin channels through the G-Ware software.
+
+The system can assume that the channels are set up for stereo, so that there are 2 channels paired 
 together.  If stereo=1, the module will take each action twice, once on the listed source/zone number and again on the source/zone + 1.
 
-For each source or zone, indicate the channel to use on the XAP unit.
+For each source or zone, multiple channels can be listed, as a list.  If multiple channels are listed for a source and an output, they will be paired sequentially, source item 1 to zeone item 1, source item 2 to zone irem 2, etc.  If there are more source channels than zone channels, only the first channels in the source will be used.  If there are more channels in a zone than in the source being applied ot it, the source channels will be repeated.  This multiple channel apporach can be used to handle stereo (instead of the stereo=1 approach), but it was added to handle surround sound sources / zones. 
 
 The platform will create individual media_player controls for each source and zone.  Each source will be shown with a volume slider, adjusting the 
 gain for that input.  Each Zone will be shown with a dropbox to select from the available zones and a volume slider to adjust the gain for that output.
 
-Basic configuration:
-
+#Basic configuration
 media_player:
    - platform: xap_controller
      path: /dev/ttyUSB-XAP800
@@ -36,20 +40,60 @@ media_player:
        'Living/Dining/Library':
          - 9
        'WorkRoom':
-         - "1:11"
+         - "2:1"
      sources:
        'Home Audio': 9
        'Family TV Audio': 11
 
 
+#multi-unit example
+media_player:
+   - platform: xap_controller
+     path: /dev/ttyUSB-XAP800
+     name: MyXAP
+     scan_interval: 30
+     stereo: 0
+     baud: 9600
+     zones:
+       'Office':
+         - "1:1"
+         - "1:2"
+       'Kitchen':
+         - "1:3"
+         - "1:4"
+        'Family Room Surround':
+         - "2:1"
+         - "2:2"
+         - "2:3"
+         - "2:4"
+         - "2:1"
+         - "2:2"
+         # Family Room Surround has no center channel, so list the two front 
+         # speakers at the end and map the center channel to each of them
+
+     sources:
+       'Home Audio':
+         - "1:9:O:E"
+         - "1:10:P:E"
+       'Family TV Surround Audio':
+         - "2:1:V:E"
+         - "2:2:W:E"
+         - "2:3:X:E"
+         - "2:4:Y:E"
+         - "2:5:Z:E"
+         - "2:5:Z:E"
+         # Family Room Surround has no center channel, so list the center channel twice at the end,  
+         # then it will be connected to the last 2 itens in the zone channel list
+
+
 zones: a list of output zone names, with a list one or more outputs for each zone. 
 sources: a list of source names, with a list of one or more sources per source name.
- sources are listed as eithee a digit, indicateing the input channel on unit 0, or else a string of the 
+ sources are listed as either a digit, indicating the input channel on unit 0, or else a string of the 
 format:  "<unit#>:<input#>:<bus letter>:<bus type>. Bus and Bus type are optional, but are neeed if using more than 1
  unit and you want a source to be available on outputs in other units. 
 path: serial device path (can be a virtual serial port, using socat for example)
 name: the name of the platform instance
-stereo: 1=stereo, 0=mono  If stereo=1, each action will be performed twice on the output and output+1
+stereo: 1=stereo, 0=mono  If stereo=1, each action will be performed twice on the input (output) and input (output)+1
 baud: baud rate of serial port, default=38400
 scan_interval: how often to scane the unit for changes
 
@@ -336,6 +380,7 @@ class XAPZone(MediaPlayerDevice):
         self._active_source = SRC_OFF
         self._poweroff_source = self._active_source
         self._active_source = self.get_source()
+        self._poweroff_source = self._active_source
         # make sure sources synced across outputs
         self.select_source(self._active_source)
         self._sync_volume_level()
@@ -384,11 +429,11 @@ class XAPZone(MediaPlayerDevice):
                 self._xapx00.setMatrixRouting(XIN, XOUT, ON, inGroup = XINGRP, unitCode = XUNIT)
                 self._active_source = source
             cnt += 1
-        self._poweroff_source = self._active_source
 
     def get_source(self):
         """Get first active source for outputs in this zone
-           Since an input can be part of multiple sources, need to make this more sophisticated.
+           Since an input can be part of multiple sources, need to make this more sophisticated,
+           need to for all channels being on.
         """
         _LOGGER.debug("In get_source for {}".format(self))
         _LOGGER.debug("  Checking: {}".format(self._sources))
@@ -405,7 +450,6 @@ class XAPZone(MediaPlayerDevice):
                     self._active_source = xIn.__str__()
                     break
         _LOGGER.debug("get_source for %s = %s" % (self._name, self._active_source))
-        self._poweroff_source = self._active_source
         return self._active_source
         
     @property
@@ -493,13 +537,16 @@ class XAPZone(MediaPlayerDevice):
 
     def turn_on(self):
         """Turn zone on"""
+        _LOGGER.debug("turn_on {}".format(self))
         self._state = STATE_ON
         self.select_source(self._poweroff_source)
         self.mute_volume(0)
 
     def turn_off(self):
         """Turn off zone"""
+        _LOGGER.debug("turn_off {}".format(self))
         self._state = STATE_OFF
+        self._poweroff_source = self._active_source
         self.select_source(SRC_OFF)
         self.mute_volume(1)
 
